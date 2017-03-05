23a16d141489023f8e0b6f941cd7d2d1f183cd58
Fredrik Carlsson <fredrik.carlsson.92@gmail.com>
2017-03-05 17:38:08 +0100

    Added code in the DataAnalyser class (avrageSpeed()) and a corresponding testbench method

--- STEVE/STEVE/Source files cpp/DataAnalyser.cs
+++ STEVE/STEVE/Source files cpp/DataAnalyser.cs
@@ -8,5 +8,20 @@ namespace STEVE.Source_files_cpp
 {
     class DataAnalyser
     {
+        /// <summary>
+        /// Returns the avrage speed traveled
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public int avrageSpeed()
+        {
+            /* Placeholder for better implementation
+               totlaDist and totalTimeSec should 
+               come from a function call */
+            int totalDist = 100; 
+            int totalTimeSec = 120;
+
+            return totalDist / totalTimeSec;
+        }
     }
 }
--- STEVE/STEVE/TestBench/TestBench.cs
+++ STEVE/STEVE/TestBench/TestBench.cs
@@ -4,9 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using STEVE.Source_files_cpp;
+
 namespace STEVE.TestBench
 {
     class TestBench
     {
+        DataAnalyser dataAnalyser = new DataAnalyser(); 
+
+
+        public bool testAvrageSpeed()
+        {
+
+            return false;
+        }
     }
 }
