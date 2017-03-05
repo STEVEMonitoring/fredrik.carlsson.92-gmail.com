e63d08a66020d9fafdf122a8f8f844c8e667d421
Fredrik Carlsson <fredrik.carlsson.92@gmail.com>
2017-03-05 20:52:46 +0100

    Added some init commands and screen class for the DOGXL240-7 screen, commands are not used yet.

---
+++ STEVE/STEVE/Source files cpp/LCDdisplaySmall.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STEVE.Source_files_cpp
+{
+    class LCDdisplaySmall
+    {
+        public void lcdInit()
+        {
+            byte CMD_COM_END_H = 0xF1;               
+            byte CMD_COM_END_L = 0x67;                
+            byte CMD_MAPPING_CTRL = 0xC0;      
+            byte CMD_SCROLL_LINE_H = 0x40;               	
+            byte CMD_SCROLL_LINE_L = 0x50;         
+            byte CMD_PANEL_LOADING_38NF = 0x2B;            
+            byte CMD_BIAS_RATIO_12 = 0xEB;                
+            byte CMD_CONTRAST = 0x81;           
+            byte LCD_CONTRAST_VALUE = 0x5F;	 
+            byte CMD_RAM_ADDR_AUTO = 0x89;	    
+            byte CMD_DISPLAY_ON = 0xAF;     
+            
+            
+        }
+
+        public void writeLcd(string message)
+        {
+
+        }
+    }
+}
